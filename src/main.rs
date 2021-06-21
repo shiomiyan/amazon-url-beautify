81fbe79cdfc78aefc902fa395bd747cccec79c10
shiomiyan <dev.umisk@gmail.com>
2021-06-21 20:47:06 +0900

    add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 mod url;
 
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("AmazonUrlBeautify")
+        .version("1.0")
+        .author("@shiomiyan")
+        .about("Simple CLI tool for beautify Amazon URL.")
+        .arg(
+            Arg::with_name("URL")
+                .help("Product URL on Amazon.co.jp.")
+        )
+        .get_matches();
+
+        if let Some(url) = matches.value_of("URL") {
+            let result = url::parser::get_asin(url);
+            println!("https://www.amazon.co.jp/dp/{}", result);
+        }
 }
